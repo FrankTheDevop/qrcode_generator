84da3ae562933a650c903a14181827348e09c441
Frank-Dieter Stadtler <frank-dieter.stadtler@gmx.de>
2019-08-18 16:26:16 +0700

    Return qr code image as buffer

--- common/models/system.js
+++ common/models/system.js
@@ -1,18 +1,39 @@
 'use strict'
 
 const Promise = require('bluebird')
+const qr = require('qr-image')
+const fs = require('fs')
+const path = require('path')
 
 const logger = require('../../server/modules/getLogger')('models/system.js')
 const helper = require('../../server/modules/helper')
 
 module.exports = function (System) {
-  System.consumerStart = function (payload) {
-    const Jobs = Post.app.models.job
-    payload = helper.checkPayload(payload)
+  System.generateQrCode = async (email, token) => {
+    const QRCode = System.app.models.qr_code
 
-    logger.debug(payload)
-    const {dt, a} = payload
+    const exists = await QRCode.existsAlready(email, token)
 
-    return Promise.resolve()
+    if (!exists) {
+      await QRCode.generate(email, token)
+      await QRCode.saveIt(email, token)
+    }
+
+    return QRCode.generatePublicPath(email, token)
+  }
+
+  System.getQRCode = async (email) => {
+    const QRCode = System.app.models.qr_code
+
+    const entry = await QRCode.findByEmail(email)
+
+    if (helper.doWeNeedToGetTheEntry(entry)) {
+      throw new Error('QR Code not yet generated')
+    }
+
+    const systemData = await QRCode.getFile(entry.email, entry.token)
+
+    console.log('systemData:', systemData)
+    return systemData
   }
 }
